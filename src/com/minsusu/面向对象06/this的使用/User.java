07f296322df7891494df4cb750a9ec6a436b3aa9
min <mxj12315@qq.com>
2021-01-14 12:29:59 +0800

    java20210114

--- src/com/minsusu/面向对象06/this的使用/User.java
+++ src/com/minsusu/面向对象06/this的使用/User.java
@@ -4,6 +4,8 @@ package com.minsusu.面向对象06.this的使用;
 public class User {
     String name = "java";  // 实例属性
     static String _name = "User";  // 类属性
+
+
     public static void main(String[] args) {
         // 访问静态方法
         User._sum();
@@ -12,7 +14,7 @@ public class User {
         User u = new User();
         u.sum();
     }
-    /*
+    /**
      * static修饰的方法中没有this
      */
     public static void _sum(){
@@ -23,6 +25,9 @@ public class User {
         System.out.println("这是静态方法中访问实例属性"+u.name);
     }
 
+    /**
+     * 没有static修饰的实例方法
+     */
     public void sum(){
         System.out.println(_name);
         String names = this.name;
