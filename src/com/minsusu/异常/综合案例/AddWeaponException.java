19ffcb9a36dfdedf576a976b88a5df00ff641794
min <mxj12315@qq.com>
2021-01-19 16:10:17 +0800

    java20210116 Exception

--- src/com/minsusu/异常/综合案例/AddWeaponException.java
+++ src/com/minsusu/异常/综合案例/AddWeaponException.java
@@ -2,6 +2,9 @@ package com.minsusu.异常.综合案例;
 
 import org.omg.CORBA.PUBLIC_MEMBER;
 
+/**
+ * 自定义异常对象
+ */
 public class AddWeaponException extends Exception{
     public AddWeaponException()
     {
